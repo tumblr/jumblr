9b558c9aea3109a67326fdc347bcf2f3851a7fa9
John Crepezzi <john.crepezzi@gmail.com>
2013-02-17 21:51:09 -0500

    Remove credentials

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,23 +1,38 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.tumblr.jumblr.JumblrClient;
 import com.tumblr.jumblr.types.Blog;
 import com.tumblr.jumblr.types.Post;
 import com.tumblr.jumblr.types.User;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        FileReader fr = new FileReader("credentials.json");
+        BufferedReader br = new BufferedReader(fr);
+        
+        String json = "";
+        while (br.ready()) { json += br.readLine(); }
+        
+        JsonParser parser = new JsonParser();
+        JsonObject obj = (JsonObject) parser.parse(json);
         
         JumblrClient client = new JumblrClient(
-            "consumer_key",
-            "consumer_secret"
+            obj.getAsJsonPrimitive("consumer_key").getAsString(),
+            obj.getAsJsonPrimitive("consumer_secret").getAsString()
         );
         
         client.setToken(
-            "oauth_token",
-            "oauth_token_secret"
-        );
-        
+            obj.getAsJsonPrimitive("oauth_token").getAsString(),
+            obj.getAsJsonPrimitive("oauth_token_secret").getAsString()
+        );        
         
         // Play with users
         User user = client.userInfo();
