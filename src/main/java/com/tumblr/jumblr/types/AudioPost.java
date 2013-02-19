148105610d0b697255ec4dd03201a4bf9a1484ec
John Crepezzi <john.crepezzi@gmail.com>
2013-02-19 13:31:33 -0500

    Audio fields filled out

--- src/main/java/com/tumblr/jumblr/types/AudioPost.java
+++ src/main/java/com/tumblr/jumblr/types/AudioPost.java
@@ -3,6 +3,46 @@ package com.tumblr.jumblr.types;
 public class AudioPost extends Post {
 
     // @TODO make editable
-    // @TODO fill out fields
+
+    private String caption, player;
+    private int plays;
+    private String album_art, artist, album, track_name;
+    private int track_number, year;
+
+    public int getPlays() {
+        return plays;
+    }
+
+    public String getTrackName() {
+        return track_name;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbumArt() {
+        return album_art;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getTrackNumber() {
+        return track_number;
+    }
+
+    public String getCaption () {
+        return caption;
+    }
+
+    public String getEmbedCode() {
+        return player;
+    }
 
 }
