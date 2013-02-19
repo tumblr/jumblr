f01629ff4f08fb1f079ba90035a2b6bb487ee284
John Crepezzi <john.crepezzi@gmail.com>
2013-02-19 12:15:36 -0500

    Asking name & asking url on ask posts

--- src/main/java/com/tumblr/jumblr/types/AnswerPost.java
+++ src/main/java/com/tumblr/jumblr/types/AnswerPost.java
@@ -4,8 +4,17 @@ public class AnswerPost extends Post {
 
     // @TODO make editable
 
+    private String asking_name, asking_url;
     private String question;
     private String answer;
+
+    public String getAskingUrl() {
+        return asking_url;
+    }
+
+    public String getAskingName() {
+        return asking_name;
+    }
 
     public String getQuestion() {
         return question;
