c51dd27586be0b8a8d6e5f7e00f7d691b1881127
Jackson <shades97@gmail.com>
2013-05-29 16:46:51 -0400

    Cleaned up StaticServer

--- src/main/java/com/tumblr/jumblr/request/StaticServer.java
+++ src/main/java/com/tumblr/jumblr/request/StaticServer.java
@@ -1,21 +1,22 @@
 package com.tumblr.jumblr.request;
 
-import com.sun.net.httpserver.*;
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.ListMultimap;
-import java.io.*;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+// The Javadocs for the Sun httpserver stuff can be found at
+// http://docs.oracle.com/javase/6/docs/jre/api/net/httpserver/spec/com/sun/net/httpserver/package-summary.html
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URLEncodedUtils;
 
 /**
  * An HTTP server that returns a static page and offers up the requests that have been sent to it.
  * 
- * NOTE: I modified this from http://stackoverflow.com/a/3732328/1772907
+ * Note: I modified this from http://stackoverflow.com/a/3732328/1772907
  * 
  * @author Jackson
  */
@@ -24,17 +25,24 @@ public class StaticServer {
 
     private final List<URI> requests;
     private final HttpServer server;
+    
+    public static final int DEFAULT_PORT = 8000;
 
     /**
-     * Creates a server to listen on the given port and path.
-     * 
+     * Creates a server to listen on the given port and path that returns the given static HTML response page to every
+     * caller.
      * @param listenPort port to bind to
-     * @param listenPath path to listen to (default "/callback")
-     * @param responsePageLocation (default "callback_response_page.html")
+     * @param listenPath path to listen to
      * @throws IOException 
      */
     public StaticServer(int listenPort, String listenPath, String responsePage) throws IOException {
-        this.server = HttpServer.create(new InetSocketAddress(listenPort), 0);
+        InetSocketAddress address;
+        try {
+            address = new InetSocketAddress(listenPort);
+        } catch (IllegalArgumentException e) {
+            address = new InetSocketAddress(DEFAULT_PORT);
+        }
+        this.server = HttpServer.create(address, -1); // -1 specifies with system-default backlog
         server.createContext(listenPath, new RequestHandler(this, responsePage));
         server.setExecutor(null); // creates a default executor
         server.start();
@@ -42,6 +50,13 @@ public class StaticServer {
         this.requests = Collections.synchronizedList(new LinkedList<URI>());
     }
     
+    /**
+     * Creates a server to listen for requests made to the given url, but with any parameters. Returns the given static
+     * HTML response page to every caller.
+     * @param url
+     * @param responsePage
+     * @throws IOException
+     */
     public StaticServer(URI url, String responsePage) throws IOException {
         this(url.getPort(), url.getPath(), responsePage);
     }
@@ -57,8 +72,6 @@ public class StaticServer {
         }
 
         public void handle(HttpExchange t) throws IOException {
-            String response = readResource("callback_response_page.html");
-
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
@@ -69,57 +82,41 @@ public class StaticServer {
     }
 
     private void addRequest(URI uri) {
-        
         requests.add(uri);
     }
     
+    /**
+     * Removes and returns the next request in the list. Returns null if the list is empty.
+     * @return The next request.
+     */
+    public URI getRequest() {
+        if (requests.isEmpty()) {
+            return null;
+        }
+        return requests.remove(0);
+    }
+    
+    /**
+     * Removes and returns the next request in the list. If the list is empty, waits for the next request.
+     * Warning: this method blocks not only on the network, but on user input.
+     * @return The next request.
+     */
     public URI waitForNextRequest() {
-        while (requests.isEmpty()) {
+        URI request = null;
+        while ((request = getRequest()) == null) {
             try {
-                Thread.sleep(100);
+                Thread.sleep(100); // 100 milliseconds, or 0.1 seconds
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        return requests.get(0);
+        return request;
     }
     
+    /**
+     * Stops the server. You should call this when you are done, or the port will stay blocked.
+     */
     public void stop() {
         server.stop(1);
     }
-<<<<<<< HEAD:src/main/java/com/tumblr/jumblr/request/CallbackServer.java
-    
-    /**
-     * Get all the parameters from a given URI.
-     * @param url the url to get the parameters from
-     * @return all the parameters and values
-     */
-    static ListMultimap<String, String> getUrlParameters(URI url) {
-        ListMultimap<String, String> ret = ArrayListMultimap.create();
-        for (NameValuePair param : URLEncodedUtils.parse(url, "UTF-8")) {
-            ret.put(param.getName(), param.getValue());
-        }
-        return ret;
-    }
-
-    /**
-     * Reads the given resource, even in jar packaging.
-     * @param name the name of the resource
-     * @return the entire text of the resource
-     * @throws IOException
-     */
-    private static String readResource(String name) throws IOException {
-        InputStream is = ClassLoader.getSystemResourceAsStream(name);
-        Reader r = new BufferedReader(new InputStreamReader(is));
-    
-        StringBuffer sb = new StringBuffer();
-        while (r.ready()) {
-            sb.append((char) r.read());
-        }
-        r.close();
-    
-        return sb.toString();
-    }
-=======
->>>>>>> Made the authentication system return false on errors, renamed CallbackServer to StaticServer:src/main/java/com/tumblr/jumblr/request/StaticServer.java
 }
